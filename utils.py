130781d3583965e4ef4139b544021f87256be7ec
SelkieAnna <monstrolog@gmail.com>
2018-04-08 01:49:00 +0300

    edited server

--- utils.py
+++ utils.py
@@ -2,3 +2,9 @@ import datetime
 
 def convert_to_date_time(unixtime):
     return datetime.datetime.fromtimestamp(int(unixtime).strftime('%Y-%m-%d %H:%M:%S')
+
+def file_len(fname):
+    with open(fname) as f:
+        for i, l in enumerate(f):
+            pass
+    return i + 1
